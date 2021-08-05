686e93726694369f420a0e5bb5fe30a4d51e7bac
sohanr <mizan.rahman66d@gmail.com>
2021-08-05 08:02:57 +0600

    static n dynamic

---
+++ Array/static vs dynamic.js
@@ -0,0 +1,10 @@
+// static arrays = fixed in size 
+//dynamic arrays = allow us to copy and rebuild an array at a new location which with more memory.
+
+
+// static
+
+// in c++ 
+// int a[20]
+
+//in javascript we never really allocated memory in arrays. so, they work like dynamic arrays. They automatically allocate.
